1d62bbf4234c730640f2aecf9e00da9b39af55c0
Greg Damico <greg.damico@flatironschool.com>
2022-06-07 13:20:26 -0700

    commit tree 2e11689682ffdeafecb580231cf035c1a06c894b

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  if (distance <= 400){
+    return 'This one is on me!'
+  } else if (distance <= 2000) {
+    return 'That will be twenty bucks.'
+  } else if (distance <= 2500){
+    return 'I will gladly take your thirty bucks.'
+  } else {return 'No can do.'}
+
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  let out;
+  city === 'NYC' ? (out = 'Ok, sounds good.') : (out = 'No go.')
+  return out;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  let out;
+  switch (tip) {
+    case 'generous': out = 'Thank you so much.';
+    break;
+    case 'not as generous': out = 'Thank you.';
+    break;
+    default : out = 'Bye.'
+  }
+  return out;
 }
